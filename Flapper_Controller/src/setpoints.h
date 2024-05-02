a1d50120f135ed1cce6c79cd2716ef47e4e9f1d2
felix-kocht <159034498+felix-kocht@users.noreply.github.com>
2024-05-02 23:11:45 +0200

    minor change

--- Flapper_Controller/src/setpoints.h
+++ Flapper_Controller/src/setpoints.h
@@ -1,9 +1,9 @@
 #ifndef SETPOINTS_H
 #define SETPOINTS_H
 
-void updateSineWaves(float (*params)[5], float (*targets), float time);
+void updateSineWaves(float (*params)[5], float (*targets), float time); 
 
-float sineWave(float amplitude, float frequency, float phase, float offset = 0.0, float deadband_low = 0.0, float deadband_high = 0.0, float time);
+float sineWave(float amplitude, float frequency, float phase, float offset = 0.0, float deadband_low = 0.0, float deadband_high = 0.0, float time); //TODO: hide this function in the .h file
 
 //just mock these thrust and turn_torque values, if on the lake (a bit of calibration and it should be fine)
 void parameter_tuner(float waterspeed, float thrust, float target_thrust, float turn_torque, float target_turn_torque, float (*params)[5]);
