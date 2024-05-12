c4657e91c41041e22afca8cd6fa7514996efb519
felix-kocht <159034498+felix-kocht@users.noreply.github.com>
2024-05-12 16:59:35 +0200

    Add WiFi and web server functionality

--- Data transmission controller/src/main.cpp
+++ Data transmission controller/src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char* ssid = "ESP8266-AP";
+const char* password = "password";
+
+ESP8266WebServer server(80);
+
+void handleRoot() {
+  server.send(200, "text/plain", "Hello from ESP8266!");
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  WiFi.softAP(ssid, password);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP Address: ");
+  Serial.println(myIP);
+
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  server.handleClient();
 }
