23e4a8017d3a8e6a4699d8b3e999ba1ec3886ae1
felix-kocht <159034498+felix-kocht@users.noreply.github.com>
2024-06-09 16:45:00 +0200

    pseudocode for IO Master

--- Visual_Detector/IOMaster.py
+++ Visual_Detector/IOMaster.py
@@ -1,14 +1,16 @@
-#imports
+# imports
 
-#variables to set by user
-##csvfile
+# variables to set by user
+#   csvfile to save to
+#   list of instructions to send to io managers (and when)
+#       e.g. set of paramters and correspoding duration (or send-out time) for each serial device
+#   class definitino stuff for io manager
 
-#class definition for a io manager
-##port, baudrate, parse pattern,
-##read
-##write
+# class definition for an io manager
+#   port, baudrate, parse pattern,
+#   read
+#   write
 
-#function to save to csv file 
+# function to save to csv file
 
-#main function, creating 2 io managers
-
+# main function, creating 2 / 3 io managers
