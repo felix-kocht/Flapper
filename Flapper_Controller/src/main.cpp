97b98868dbbeab32368aeaf518136991cfe09a46
felix-kocht <159034498+felix-kocht@users.noreply.github.com>
2024-06-12 18:43:02 +0200

    now only prints very relevant stuff

--- Flapper_Controller/src/main.cpp
+++ Flapper_Controller/src/main.cpp
@@ -76,8 +76,8 @@ void loop() {
     camber_servo_right.write(setpoints[3]);
     camber_servo_left.write(setpoints[4]);
 
-    // Print values for debugging
-    float valuesToPrint[] = {setpoints[0], setpoints[1], setpoints[2], setpoints[3], setpoints[4]};
+    // Print values for debugging: Heave, Power consumption, noise
+    float valuesToPrint[] = {setpoints[0], 0, 0};
     int length = sizeof(valuesToPrint) / sizeof(valuesToPrint[0]);
     print_floats(valuesToPrint, length);
 }
