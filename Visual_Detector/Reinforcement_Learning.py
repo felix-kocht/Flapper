915e034c6aa5c144b9ff07327451559b6f7bf780
felix-kocht <159034498+felix-kocht@users.noreply.github.com>
2024-07-07 23:32:22 +0200

    RL works with dummy rewards

--- Visual_Detector/Reinforcement_Learning.py
+++ Visual_Detector/Reinforcement_Learning.py
@@ -3,7 +3,7 @@ from skopt.space import Real
 from skopt import gp_minimize
 
 parameters = {
-    'frequency': [0.2, 1],
+    'frequency': [0.2, 0.6], #TODO: increase if safe to do so
     'pitch amplitude': [10, 80],
     'pitch phase': [-1.57, 1.57],
     'camber amplitude': [0, 60],
@@ -11,38 +11,17 @@ parameters = {
 }
 
 initial_guess = {
-    'frequency': 0.6,
-    'pitch amplitude': 45,
+    'frequency': 0.5,
+    'pitch amplitude': 60,
     'pitch phase': 0,
-    'camber amplitude': 30,
+    'camber amplitude': 50,
     'camber phase': 0,
 }
 
-max_tests = 10
+max_tests = 4
 test_durations = 5
 
 # state space, all ranging from 0 to 1
-normalized_parameters = [0,0,0,0,0]
-
-#TODO: find best reward function
-def reward_function(thrust,consumption):
-    if consumption == 0:
-        return 0.1*thrust
-    return thrust/consumption + 0.1*thrust
-
-#initializing on the 0-1 range
-def initialize_parameters():
-    i = 0
-    for key in parameters.keys():
-        full_range = parameters[key][1] - parameters[key][0]
-        center = parameters[key][0] + 0.5 * full_range
-        normalized_parameters[i] = (
-            initial_guess[key] - center)/full_range + 0.5
-        i += 1
-
-
-""" 
-# Define the search space for the 5 parameters
 space = [
     Real(0.0, 1.0, name='param1'),
     Real(0.0, 1.0, name='param2'),
@@ -51,9 +30,36 @@ space = [
     Real(0.0, 1.0, name='param5')
 ]
 
+#in space range (0 to 1): x0 is  initial guess, y0 the initial reward
+x0 = [0,0,0,0,0]
+#y0 = -16 #TODO: fill in if available
+
+#TODO: find best reward function
+def reward_function(thrust,consumption):
+    if consumption == 0:
+        return 0.1*thrust
+    return thrust/consumption + 0.1*thrust
+
+#initializing x0 on the 0-1 range
+def initialize_parameters():
+    i = 0
+    for key in parameters.keys():
+        full_range = parameters[key][1] - parameters[key][0]
+        center = parameters[key][0] + 0.5 * full_range
+        x0[i] = (
+            initial_guess[key] - center)/full_range + 0.5
+        i += 1
+
+# Placeholder functions for demonstration
+def run_physical_test(param1, param2, param3, param4, param5):
+    # Implement the real-world test logic here and return the measured thrust
+    return 50
+
+def measure_energy_consumption(param1, param2, param3, param4, param5):
+    # Implement the real-world test logic here and return the measured energy consumption
+    return 5
+
 # Example of an objective function interfacing with the real-world setup
-
-
 def objective(params):
     # Extract parameters
     param1, param2, param3, param4, param5 = params
@@ -64,33 +70,21 @@ def objective(params):
     thrust = run_physical_test(param1, param2, param3, param4, param5)
     consumption = measure_energy_consumption(
         param1, param2, param3, param4, param5)  # Function to measure energy
-
-    if consumption == 0:
-        return -0.1 * thrust  # Use a small negative reward to avoid division by zero
-
-    reward = (thrust / consumption) + (0.1 * thrust)
+    reward = reward_function(thrust, consumption)
     return -reward  # Minimize the negative reward to maximize the reward
 
+# Initialize the parameters
+initialize_parameters()
+print(x0)
 
 # Perform Bayesian Optimization
-res = gp_minimize(objective, space, n_calls=50, random_state=0)
+res = gp_minimize(objective, space, n_calls=max_tests, random_state=0, verbose = True, n_initial_points = 3, x0 = x0)#, y0=y0)
 
 # Output the best parameters found
 print("Best parameters: ", res.x)
 print("Best reward: ", -res.fun)  # Since we minimized the negative reward
+#print("details: ", res)
 
-# Placeholder functions for demonstration
-
-
-def run_physical_test(param1, param2, param3, param4, param5):
-    # Implement the real-world test logic here and return the measured thrust
-    pass
-
-
-def measure_energy_consumption(param1, param2, param3, param4, param5):
-    # Implement the real-world test logic here and return the measured energy consumption
-    pass
- """
 
 # IO: #TODO: modify or create version of these files that allow for easy calling and input parameters, e.g. name of test case
 #TODO: or maybe just implement the naming of test cases via the csv file anyways
@@ -99,7 +93,3 @@ def measure_energy_consumption(param1, param2, param3, param4, param5):
 # calls result_eval.py to evaluate the results of the testrun
 # reads the test_case csv file
 
-# RL:
-# method to decide on next test case (gradient descent with the best available gradient?) --> weigh between exploration and exploitation, use some tested method for this
-# then i would have to randomly sample some test cases to get the gradient: either completely random, or assume inital guess is the best and sample around it
-#
