c9d0d04a53a0b40cf27f25ef2038baaf65d5dc45
felix-kocht <159034498+felix-kocht@users.noreply.github.com>
2024-08-26 08:49:04 +0200

    some plotting

--- Visual_Detector/plot_rl_steps.py
+++ Visual_Detector/plot_rl_steps.py
@@ -2,7 +2,7 @@ import csv
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-input_file = 'RL_output_force.csv'
+input_file = 'RL_output_efficiency.csv'
 # Read the data from the CSV file
 
 iterations = []
@@ -16,7 +16,7 @@ with open(input_file, 'r') as csvfile:
     for row in csvreader:
         iterations.append(int(row[0]))
         # Assuming Pitch Amplitude is the 4nd column
-        #frequencies.append(float(row[3]))
+        # frequencies.append(float(row[3]))
         # Assuming Frequency is the 2nd column
         frequencies.append(float(row[1]))
         # Assuming Pitch Phase is the 6th column
@@ -33,7 +33,7 @@ sc = ax.scatter(frequencies, pitch_phases, function_values,
                 c=iterations, cmap='cool')
 
 # Connect the dots with lines according to adjacent iterations
-#ax.plot(frequencies, pitch_phases, function_values, color='gray', alpha=0.5)
+# ax.plot(frequencies, pitch_phases, function_values, color='gray', alpha=0.5)
 
 # Add color bar
 cbar = plt.colorbar(sc, ax=ax)
